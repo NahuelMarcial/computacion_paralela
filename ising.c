defa055ad35e5d14e0fb349049876d2b5f5d11c9
NahuelMarcial <nahuel.marcial@mi.unc.edu.ar>
2025-04-16 15:45:01 -0300

    expf_lookup_table

--- ising.c
+++ ising.c
@@ -4,32 +4,24 @@
 #include <math.h>
 #include <stdlib.h>
 
+static struct {
+    float temp;
+    float table[5]; // EXPF_LOOKUP_TABLE_SIZE = 5 (for half_delta_E from 0 to 4)
+} expf_lookup = {0.0, {0.0}};
+
+static void init_expf_lookup_table(const float temp)
+{
+    expf_lookup.temp = temp;
+    for (int half_delta_E = 1; half_delta_E <= 4; ++half_delta_E) {
+        expf_lookup.table[half_delta_E] = expf(-(half_delta_E * 2) / temp);
+    }
+}
 
 void update(const float temp, int grid[L][L])
 {
     // typewriter update
     for (unsigned int i = 0; i < L; ++i) {
         for (unsigned int j = 0; j < L; ++j) {
-            int spin_old = grid[i][j];
-            int spin_new = (-1) * spin_old;
-
-            // Sin optimizacion
-            // computing h_before
-            int spin_neigh_n = grid[(i + L - 1) % L][j];
-            int spin_neigh_e = grid[i][(j + 1) % L];
-            int spin_neigh_w = grid[i][(j + L - 1) % L];
-            int spin_neigh_s = grid[(i + 1) % L][j];
-            int h_before = -(spin_old * spin_neigh_n) - (spin_old * spin_neigh_e) - (spin_old * spin_neigh_w) - (spin_old * spin_neigh_s);
-
-            // h after taking new spin
-            int h_after = -(spin_new * spin_neigh_n) - (spin_new * spin_neigh_e) - (spin_new * spin_neigh_w) - (spin_new * spin_neigh_s);
-
-            int delta_E = h_after - h_before;
-            float p = rand() / (float)RAND_MAX;
-
-
-            // Con optimizacion
-            /*
             int spin_old = grid[i][j];
             int spin_new = (-1) * spin_old;
 
@@ -40,24 +32,20 @@ void update(const float temp, int grid[L][L])
 
             int delta_E = 2 * spin_old * (spin_neigh_n + spin_neigh_e + spin_neigh_w + spin_neigh_s);
             float p = generate_random();
-            */
 
-            if (delta_E <= 0 || p <= expf(-delta_E / temp)) {
-                grid[i][j] = spin_new;
+            if (delta_E <= 0 || p <= expf_lookup.table[half_delta_E]) {
+                write[idx(x,y)] = spin_new;
             }
         }
     }
 }
-
-// for (unsigned int j = i%2; j < L; j+=2) { // Con optimizacion
-//return -((double)E); // Con optimizacion
 
 double calculate(int grid[L][L], int* M_max)
 {
     int E = 0;
     for (unsigned int i = 0; i < L; ++i) {
         
-        for (unsigned int j = 0; j < L; ++j) {
+        for (unsigned int j = i%2; j < L; j+=2) {
             int spin = grid[i][j];
             int spin_neigh_n = grid[(i + 1) % L][j];
             int spin_neigh_e = grid[i][(j + 1) % L];
@@ -68,6 +56,6 @@ double calculate(int grid[L][L], int* M_max)
             *M_max += spin;
         }
     }
-    return -((double)E / 2.0);
+    return -((double)E);
     
 }
