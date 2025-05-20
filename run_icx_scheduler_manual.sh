752d0d9086195d0d69e553e6adc2b5420c281743
NahuelMarcial <nahuel.marcial@mi.unc.edu.ar>
2025-05-20 14:49:57 -0300

    Scripts

---
+++ run_icx_scheduler_manual.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+export OMP_PROC_BIND=close
+source run.sh
+
+SCHEDULER_TYPE="static" # Cambia esto a "dynamic" o "guided" según el ganador
+
+echo "Métrica;Comentario"
+
+for THREADS in 1 2 3 4 5 6 7
+do
+    run icx "-Ofast -flto -march=native -ftree-vectorize -DL=2048 -DSCHEDULE_TYPE=$SCHEDULER_TYPE -fopenmp -DMAX_THREADS=$THREADS" ""; "icx $SCHEDULER_TYPE $THREADS threads"
+    echo "$OUTPUT"
+    cat /proc/loadavg
+done
---
+++ run_icx_schedulers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+export OMP_PROC_BIND=close
+source run.sh
+
+echo "Métrica;Comentario"
+
+for THREADS in 1 2 3 4 5 6 7
+do
+    run icx "-Ofast -flto -march=native -ftree-vectorize -DL=2048 -DSCHEDULE_TYPE=static -fopenmp -DMAX_THREADS=$THREADS" ""; "icx static $THREADS threads"
+    echo "$OUTPUT"
+    run icx "-Ofast -flto -march=native -ftree-vectorize -DL=2048 -DSCHEDULE_TYPE=dynamic -fopenmp -DMAX_THREADS=$THREADS" ""; "icx dynamic $THREADS threads"
+    echo "$OUTPUT"
+    run icx "-Ofast -flto -march=native -ftree-vectorize -DL=2048 -DSCHEDULE_TYPE=guided -fopenmp -DMAX_THREADS=$THREADS" ""; "icx guided $THREADS threads"
+    echo "$OUTPUT"
+    cat /proc/loadavg
+done
